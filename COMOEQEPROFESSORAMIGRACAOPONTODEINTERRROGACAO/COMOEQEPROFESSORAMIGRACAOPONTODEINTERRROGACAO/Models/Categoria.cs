c836fa08b8c0e357f949831779ca0dde3b95bcc8
andisu <Anderson_rockX@hotmail.com>
2022-02-23 22:20:30 -0300

    vamo embora por favor
    please

---
+++ COMOEQEPROFESSORAMIGRACAOPONTODEINTERRROGACAO/COMOEQEPROFESSORAMIGRACAOPONTODEINTERRROGACAO/Models/Categoria.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace COMOEQEPROFESSORAMIGRACAOPONTODEINTERRROGACAO.Models
+{
+    public class Categoria
+    {
+        [Key]   
+        public int id { get; set; }
+        
+        public string description { get; set; }
+
+        public Categoria(int id, string description)
+        {
+            this.id = id;
+            this.description = description;
+        }
+    }
+}
---
+++ COMOEQEPROFESSORAMIGRACAOPONTODEINTERRROGACAO/COMOEQEPROFESSORAMIGRACAOPONTODEINTERRROGACAO/Models/Contexto.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace COMOEQEPROFESSORAMIGRACAOPONTODEINTERRROGACAO.Models
+{
+    public class Contexto : DbContext
+    {
+     
+            public Contexto(DbContextOptions options): base(options)
+        {
+
+        }
+
+        public  DbSet<Categoria> Categorias { get; set; }
+        //injeção de dependencia tira a responsabilidade da classe de instanciar objetos, deixando a cargo da aplicação essa função(de instanciar o objeto);
+    }
+}
